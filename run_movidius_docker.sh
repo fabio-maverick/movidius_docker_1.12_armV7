96d6eb40e452d301c9b996aa2d945693345f9716
fabio-maverick <fabio.magalhaes@gmail.com>
2018-06-11 17:27:40 -0300

    update

--- run_movidius_docker.sh
+++ run_movidius_docker.sh
@@ -25,7 +25,7 @@ NCSDK_ID=`docker ps -aqf "name=^/${NCSDK_NAME}$"`
 if [ -z "${NCSDK_ID}" ]; then
     echo "Creating new redtail container."
     xhost +
-    docker run -it --privileged --network=host -v /dev:/dev -v ${HOST_DATA_DIR}:${CONTAINER_DATA_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix${DISPLAY} -p 14556:14556/udp --name=${NCSDK_NAME} ncsdk:ubuntu16.04 bash
+    docker run -it --privileged --network=host -v /dev:/dev -v ${HOST_DATA_DIR}:${CONTAINER_DATA_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=${DISPLAY} -p 14556:14556/udp --name=${NCSDK_NAME} ncsdk:ubuntu16.04 bash
 else
     echo "Found redtail container: ${NCSDK_ID}."
     # Check if the container is already running and start if necessary.
